4ae74913895541f31ee0c3a3e2666719dea296ef
Luis Perez <lp1134@hotmail.com>
2015-08-27 17:41:55 -0500

    favicon

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
-
-// Serve favicon
-app.use(favicon(__dirname + '/public/images/favicon-96x96.png'));
-
 
 var routes = require('./routes/home');
 var hardline = require('./routes/hardline');
